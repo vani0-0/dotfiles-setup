e2d33ffab963effe184d4b9eebddb7a554be6497
vani0-0 <van973vv@gmail.com>
2026-07-10 21:46:27 +0800

    new packages

---
+++ lua/vandev/plugins/gitlens.lua
@@ -0,0 +1,8 @@
+return {
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup()
+
+		vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>")
+	end,
+}
