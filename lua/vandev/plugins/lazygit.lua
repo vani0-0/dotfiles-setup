50170348b290a80f706214d39141443fae7497be
Vince^^ <111576374+vani0-0@users.noreply.github.com>
2026-07-13 08:13:36 +0800

    Delete lua/vandev directory

--- lua/vandev/plugins/lazygit.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"kdheepak/lazygit.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>")
-	end,
-}
--- lua/vandev/remap.lua
+++
@@ -1,8 +0,0 @@
--- Primeagen: "so that we don't have to retype Ex everytime and then."
-
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- while in normal mode, if i press leader pv, execute the command:
-
-vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>")
-vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>")
-vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>")
